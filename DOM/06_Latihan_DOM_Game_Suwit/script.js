f6f583ebdea81ae1538a198e8ae4afa82a2d8b37
TariqRiza <trqriza@gmail.com>
2022-04-22 19:26:33 +0700

    Membuat Latihan Game Suwit

---
+++ DOM/06_Latihan_DOM_Game_Suwit/script.js
@@ -0,0 +1,95 @@
+// Logika Peraturan Suwit
+function getPilihanComputer() {
+    // menangkap pilihan computer
+    // generate bilangan random
+    const comp = Math.random();
+
+    if (comp < 0.34) return 'gajah';
+    if (comp >= 0.34 && comp < 0.67) return 'orang';
+    return 'semut';
+}
+
+function getHasil(comp, player) {
+    if (player == comp) return 'SERI!';
+    if (player == 'gajah') return (comp == 'orang') ? 'MENANG!' : 'KALAH!';
+    if (player == 'orang') return (comp == 'semut') ? 'MENANG!' : 'KALAH!';
+    if (player == 'semut') return (comp == 'gajah') ? 'MENANG!' : 'KALAH!';
+}
+
+// Function gambar acak saat ditekan
+function putar() {
+    const imgComputer = document.querySelector('.img-computer');
+    const gambar = ['gajah', 'semut', 'orang'];
+    let i = 0;
+    const waktuMulai = new Date().getTime();
+    setInterval(function () {
+        if (new Date().getTime() - waktuMulai > 1000) {
+            clearInterval;
+            return;
+        }
+        imgComputer.setAttribute('src', 'img/' + gambar[i++] + '.png');
+        if (i == gambar.length) i = 0;
+    }, 100)
+}
+
+
+// Event Saat tombol ditekan
+// Solusi yang kurang efektif
+// const pGajah = document.querySelector('.gajah');
+// pGajah.addEventListener('click', function () {
+//     const pilihanComputer = getPilihanComputer();
+//     const pilihanPlayer = pGajah.className;
+//     const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+//     const imgComputer = document.querySelector('.img-computer');
+//     imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+//     const info = document.querySelector('.info');
+//     info.innerHTML = hasil;
+// });
+
+// const pOrang = document.querySelector('.orang');
+// pOrang.addEventListener('click', function () {
+//     const pilihanComputer = getPilihanComputer();
+//     const pilihanPlayer = pOrang.className;
+//     const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+//     const imgComputer = document.querySelector('.img-computer');
+//     imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+//     const info = document.querySelector('.info');
+//     info.innerHTML = hasil;
+// });
+
+// const pSemut = document.querySelector('.semut');
+// pSemut.addEventListener('click', function () {
+//     const pilihanComputer = getPilihanComputer();
+//     const pilihanPlayer = pSemut.className;
+//     const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+//     const imgComputer = document.querySelector('.img-computer');
+//     imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+//     const info = document.querySelector('.info');
+//     info.innerHTML = hasil;
+// });
+
+// Solusi yang lebih efektif
+const pilihan = document.querySelectorAll('li img');
+pilihan.forEach(function (pil) {
+    pil.addEventListener('click', function () {
+        const pilihanComputer = getPilihanComputer();
+        const pilihanPlayer = pil.className;
+        const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+        putar();
+
+        setTimeout(function () {
+            const imgComputer = document.querySelector('.img-computer');
+            imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+            const info = document.querySelector('.info');
+            info.innerHTML = hasil;
+        }, 1000);
+    });
+});
