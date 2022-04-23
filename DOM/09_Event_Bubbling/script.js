81be90b1eb989cda44e11e8a45d763a1455ca92f
TariqRiza <trqriza@gmail.com>
2022-04-23 15:38:13 +0700

    Membuat file Event Bubbling

---
+++ DOM/09_Event_Bubbling/script.js
@@ -0,0 +1,31 @@
+// DOM Traversal
+// const close = document.querySelectorAll('.close');
+
+// close.forEach(function (el) {
+//     el.addEventListener('click', function (e) {
+//         e.target.parentElement.style.display = 'none';
+//         // Menghindari aksi default (link tidak akan refresh saat link ditekan)
+//         e.preventDefault();
+//         // Menghentikan event sampai dititik ini saja
+//         e.stopPropagation();
+//     });
+// });
+
+// const cards = document.querySelectorAll('.card');
+// cards.forEach(function (card) {
+//     card.addEventListener('click', function () {
+//         alert('ok');
+//     });
+// });
+
+// Ketika tombol close ditekan makan event ketika cards ditekan juga berjalan
+// Karena event suatu elemen juga berpengaruh dengan elemen parentnya
+
+
+// Memberikan event di container dan mengetahui elemen apa yang kita klik di container
+const container = document.querySelector('.container');
+container.addEventListener('click', function (e) {
+    if (e.target.className == 'close') {
+        e.target.parentElement.style.display = 'none';
+    }
+});
