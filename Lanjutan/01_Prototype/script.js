e84d7e0d344bf81478434263cb1203b7fac7e138
TariqRiza <trqriza@gmail.com>
2022-04-27 07:57:31 +0700

    Mengupdate materi prototype

--- Lanjutan/01_Prototype/script.js
+++ Lanjutan/01_Prototype/script.js
@@ -90,31 +90,90 @@
 
 // 4. Object.create()
 
-const methodMahasiswa = {
-    makan: function (porsi) {
-        this.energi += porsi;
-        console.log(`Halo ${this.nama}, selamat makan!`);
-    },
-    main: function (jam) {
-        this.energi -= jam;
-        console.log(`Halo ${this.nama}, selamat bermain!`);
-    },
+// const methodMahasiswa = {
+//     makan: function (porsi) {
+//         this.energi += porsi;
+//         console.log(`Halo ${this.nama}, selamat makan!`);
+//     },
+//     main: function (jam) {
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, selamat bermain!`);
+//     },
 
-    tidur: function (jam) {
-        this.energi += jam * 2;
-        console.log(`Halo ${this.nama}, selamat tidur!`);
-    }
-};
+//     tidur: function (jam) {
+//         this.energi += jam * 2;
+//         console.log(`Halo ${this.nama}, selamat tidur!`);
+//     }
+// };
 
-function Mahasiswa(nama, energi) {
-    // let mahasiswa = {};
-    let mahasiswa = Object.create(methodMahasiswa);
+// function Mahasiswa(nama, energi) {
+//     // let mahasiswa = {};
+//     let mahasiswa = Object.create(methodMahasiswa);
 
-    mahasiswa.nama = nama;
-    mahasiswa.energi = energi;
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
 
-    return mahasiswa;
-}
+//     return mahasiswa;
+// }
 
-let riza = Mahasiswa('Riza', 17);
-let tariq = Mahasiswa('Tariq', 20);
+// let riza = Mahasiswa('Riza', 17);
+// let tariq = Mahasiswa('Tariq', 20);
+
+
+// 5. Constructor dengan Prototype Inheritance (pewarisan)
+
+// function Mahasiswa(nama, energi) {
+//     // Default
+//     // let this = Object.create(Mahasiswa.prototype);
+
+//     this.nama = nama;
+//     this.energi = energi;
+
+//     // Default
+//     // return this;
+// }
+
+// Mahasiswa.prototype.makan = function (porsi) {
+//     this.energi += porsi;
+//     return `Halo ${this.nama}, selamat makan!`;
+// }
+
+// Mahasiswa.prototype.main = function (jam) {
+//     this.energi -= jam;
+//     return `Halo ${this.nama}, selamat bermain!`;
+// }
+
+// Mahasiswa.prototype.tidur = function (jam) {
+//     thiis.energi += jam * 2;
+//     return `Halo ${this.nama}, selamat tidur!`;
+// }
+
+// let riza = new Mahasiswa('Riza', 10);
+
+
+
+// Veris dalam bentuk class
+// class Mahasiswa {
+//     constructor(nama, energi) {
+//         this.nama = nama;
+//         this.energi = energi;
+//     }
+
+//     makan(porsi) {
+//         this.energi += porsi;
+//         return `Halo ${this.nama}, selamat makan!`;
+//     }
+
+//     main(jam) {
+//         this.energi -= jam;
+//         return `Halo ${this.nama}, selamat bermain!`;
+//     }
+
+//     tidur(jam) {
+//         thiis.energi += jam * 2;
+//         return `Halo ${this.nama}, selamat tidur!`;
+//     }
+// }
+
+// let riza = new Mahasiswa('Riza', 10);
+// let tariq = new Mahasiswa('Tariq', 10);
